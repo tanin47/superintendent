dc0ba26f559882591b742c092a21bc81d5508625
Tanin Na Nakorn <tanin47@users.noreply.github.com>
2023-12-10 19:00:21 +0700

    Test (#79)
    * Add tests
    
    * Add testing
    
    * Add the initial test
    
    ---------
    
    Co-authored-by: Tanin Na Nakorn <@tanin>

---
+++ test/specs/incorrect_license.e2e.ts
@@ -0,0 +1,19 @@
+import {browser, $, expect} from '@wdio/globals'
+
+
+describe('Incorrect license', () => {
+    it('uses a incorrect license', async () => {
+        const license = "---- Superintendent license ----\n" +
+          "Email: tanin47@gmail.com\n" +
+          "Expired: 2024-09-20T23:57:20.766258645\n" +
+          "Signature:\n" +
+          "S+INCORRECT/wg4+XbGRoaHj/Z4rVIyVti8i3QabKl6vBchU8tHWq1\n" +
+          "UBHXBTCKksvLgqNaL//oXBwyGbaPQokHf60fzlLEy1qwZo9G5RmBYhvs7cHlrkJe\n" +
+          "U5QalYQc1X+chwy+c8fPizeO+ZPkPrLptlVZJJhnB/c=\n" +
+          "---- End of Superintendent license ----";
+        await $('#checkLicenseForm textarea').setValue(license);
+        await $('#checkLicenseForm button').click();
+
+        await expect($('.error-message')).toHaveText(expect.stringContaining('The license key is not valid.'));
+    });
+})
